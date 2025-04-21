78e916894392b0db84b2257708076a7185dbc3b1
imnotmatheus <matheus.f.reguta@gmail.com>
2025-04-21 19:54:38 -0300

    agrego handler para click y contador en user

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,12 +1,20 @@
 import "./user.css";
+import { useState } from "react"
 
 function User(props) {
   // retorna el JSX que luego es interpretado por REACT
   // cada vez que dentro del JSX quiero agregar variables
   // porsiones de javascript tengo que encerrar en {}
+const [contador, setContador] = useState(0)
+
+  const handlerUserClick = () => {
+    console.log(props.UserName)
+    setContador(contador + 1)
+  }
+
   return (
     // Componente Usuario
-    <li className="user-item">
+    <li className="user-item" onClick={handlerUserClick}>
       <div className="card user-item__content">
         <a href="#">
           <div className="user-item__image avatar">
@@ -15,6 +23,7 @@ function User(props) {
           <div className="user-item__info">
             <h2>Nombre: {props.UserName}</h2>
             <h3>{props.Title}</h3>
+            <h3>{contador}</h3>
           </div>
         </a>
       </div>
